7cb3f1a1c509416df6679edf1dc1b313ca19365a
martinj31 <julienmartinezf@gmail.com>
2021-03-16 16:48:01 +0100

    connexion.bdd.php à jour

--- BDD/connexion.bdd.php
+++ BDD/connexion.bdd.php
@@ -9,10 +9,19 @@ echo 'Page : connexion ';
         public function connect(){
            
           //Ne peut avoir qu'une seul instance (Singleton)
+            
+            
+            /*
+             *$nomlogin = "bd_qualif_qsf";                    // Ici, nous connectons avec le serveur local, si vous voulez le tester sur d'autre serveur, vous pouvez changer ces 3 variables
+                $nompasswd = "mYSQLQSF31";
+                $nombase = "bd_qualif_qsf";
+                $serveur = "bm124975-001.privatesql";
+                $port_bdd = "35171"; 
+             */
                 try
                {  
                     if(is_null($this->DB)){
-                        $this->DB = new PDO("mysql:host=localhost;dbname=qsf;charset=utf8","root","");
+                        $this->DB = new PDO("mysql:host=bm124975-001.privatesql;port=35171;dbname=bd_qualif_qsf;charset=utf8","bd_qualif_qsf","mYSQLQSF31");
                     }
                     return $this->DB;
                 } catch (Exception $ex) {
